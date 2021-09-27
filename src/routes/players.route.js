7f740d314b579d497c10321cc19a6ab866a7312d
unknown <formacio@PC-1062604.formacio.local>
2021-09-27 12:26:42 +0200

    chuleta

--- src/routes/players.route.js
+++ src/routes/players.route.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.send('user');
-});
+// router.get('/', function (req, res) {
+//   res.send('user');
+// });
 
+router.get('/', auth('admin'), (req, res) => {
+  res.send('funciona');
+});
 module.exports = router;
+
+/*
+1. router.post('/') => crear jugador
+2. router.put('/') => modifica el nom jugador
+3. router.post('/:id/games') => jugar partida
+4. router.delete('/:id:games') => elimina jocs del jugadore
+5. router.get('/') => retorna tots els jugadors amb percentatje exits
+6. router.get('/:id:games) => retorna llista de juagades per jugador
+*/
--- src/routes/ranking.route.js
+++ src/routes/ranking.route.js
@@ -7,3 +7,9 @@ router.get('/', function (req, res) {
 });
 
 module.exports = router;
+
+/*
+1. router.get('/') => retorna ranking de tots el jugadors per percentatge exit
+2. router.get('/loser')=> retorna pitjor precentatje d¡exit
+2. router.get('/winner')=> retorna millor precentatje d¡exit
+*/
