18fc1616eba6d4a47113ae61debdc8aecf3c4d02
unknown <formacio@PC-1062604.formacio.local>
2021-09-30 09:51:39 +0200

    added register admin function_

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { userService, tokenService, authService } = require('../services');
+const { User } = require('../models');
 
 const register = catchAsync(async (req, res) => {
   const user = await userService.createUser(req.body);
@@ -21,8 +22,15 @@ const login = catchAsync(async (req, res) => {
   res.send({ user, tokens });
 });
 
+const registerAdmin = catchAsync(async (req) => {
+  const user = await userService.createUser(req.body);
+  await tokenService.generateAuthTokens(user);
+});
+
 const loginAdmin = catchAsync(async (req, res) => {
   const { name, password } = req.body;
+  const userRegistred = await User.findOne({ name: req.body.name });
+  if (!userRegistred) registerAdmin(req);
   const user = await authService.loginUserWithNameAndPasswordAdmin(name, password);
   const tokens = await tokenService.generateAuthTokens(user);
   res.send({
